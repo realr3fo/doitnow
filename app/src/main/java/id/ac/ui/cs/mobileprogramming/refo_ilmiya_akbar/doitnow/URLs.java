ed814f833a6a4d7f088fadb635317f8f902f3841
refoilmiya <refo.ilmiya@gmail.com>
2019-10-28 23:17:55 +0700

    register, login, and profile WIP

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/refo_ilmiya_akbar/doitnow/URLs.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.mobileprogramming.refo_ilmiya_akbar.doitnow;
+
+public class URLs {
+
+    private static final String ROOT_URL = "https://doitnow-backend.herokuapp.com/api/";
+
+    public static final String URL_REGISTER = ROOT_URL + "users";
+    public static final String URL_LOGIN= ROOT_URL + "users/login";
+
+}
